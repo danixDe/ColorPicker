0dbe3699edbb67958db84a28acb2592ff91bfb10
Aravind <150506676+danixDe@users.noreply.github.com>
2024-06-14 21:04:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<ColorPicker>/',
-
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 });
